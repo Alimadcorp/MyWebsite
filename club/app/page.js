02f8fe9594ea26dae27bccad932b393dadade715
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2025-11-24 10:01:51 +0500

    Introducing: Clubs

---
+++ club/app/page.js
@@ -0,0 +1,9 @@
+import Client from "./client";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-white font-sans">
+      <Client />
+    </div>
+  );
+}

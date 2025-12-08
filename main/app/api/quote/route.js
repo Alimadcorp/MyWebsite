09c6b2b445dcf7e2fe34161490c5351e7e0d13de
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2025-12-09 01:43:55 +0500

    A bunch of quotes

--- main/app/api/quote/route.js
+++ main/app/api/quote/route.js
@@ -1,11 +1,18 @@
 import { NextResponse } from "next/server";
-
+import { quotes } from "@/data/quotes";
 const KVDB_BUCKET = process.env.KVDB_BUCKET+"/quote";
 
 export async function GET() {
   try {
-    const res = await fetch(KVDB_BUCKET, { cache: "no-store" });
-    const data = await res.json();
+    //const res = await fetch(KVDB_BUCKET, { cache: "no-store" });
+    //const data = await res.json();
+    let day = Math.floor(Date.now() / (1000 * 60 * 60 * 24)) % quotes.length;
+    const raw = quotes[day];
+    const data = {
+      quote: raw.text,
+      writer: `${raw.author}, ${raw.source}`,
+      date: new Date().setHours(0, 0, 0, 0)
+    }
     return NextResponse.json(data);
   } catch {
     return NextResponse.json({
